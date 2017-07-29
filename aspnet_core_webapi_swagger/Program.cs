94d172d6ce4439ba573f99bb9b079ad123f08bfd
Eduardo Tenório de Souza <esouza@stone.com.br>
2017-07-29 16:47:27 -0300

    Fisrst commit

---
+++ aspnet_core_webapi_swagger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspnet_core_webapi_swagger {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
