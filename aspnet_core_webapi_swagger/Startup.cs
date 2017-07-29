a6ab8099fde48545cb31188ff10d7760a4a579b0
Eduardo Tenório de Souza <esouza@stone.com.br>
2017-07-29 18:09:39 -0300

    Second commit

--- aspnet_core_webapi_swagger/Startup.cs
+++ aspnet_core_webapi_swagger/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.Swagger.Model;
 
 
 namespace aspnet_core_webapi_swagger {
@@ -24,7 +25,17 @@ namespace aspnet_core_webapi_swagger {
             services.AddMvc();
 
             services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "My API", Version = "v1" });
+                //c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title = "My API", Version = "v1" });
+
+                c.SwaggerDoc("v1",
+                    new Swashbuckle.AspNetCore.Swagger.Info {
+                        Title = "WepApi AspNet.Core",
+                        Version = "v1",
+                        Description = "Exemplo simples de WepApi AspNet.Core com documentação utilizando swagger.",
+                        TermsOfService = "None",
+                        Contact =
+                        new Swashbuckle.AspNetCore.Swagger.Contact { Name = "threenine.co.uk", Email = "support@threenine.co.uk", Url = "https://threenine.co.uk" }
+                    });
             });
         }
 
@@ -35,13 +46,12 @@ namespace aspnet_core_webapi_swagger {
 
             app.UseMvc();
 
-            app.UseSwagger(c =>
-            {
+            app.UseSwagger(c => {
                 c.PreSerializeFilters.Add((swagger, httpReq) => swagger.Host = httpReq.Host.Value);
             });
 
             app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WepApi AspNet.Core V1");
             });
         }
     }
